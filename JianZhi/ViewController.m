a1d51a7ef3bd76b02ac9488c04b4ba74b9c95c82
Monky <617907240@qq.com>
2016-02-19 10:04:19 +0800

    Initial Commit

---
+++ JianZhi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JianZhi
+//
+//  Created by Monky on 16/2/19.
+//  Copyright © 2016年 Monky. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
